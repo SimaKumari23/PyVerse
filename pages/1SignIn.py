b4efcd9fb72682b4a13daca287f0d098ac72d633
SimaKumari23 <sk3538970@gmail.com>
2026-08-03 00:17:09 +0530

    Update 1SignIn.py

--- pages/1SignIn.py
+++ pages/1SignIn.py
@@ -4,7 +4,7 @@ import pymongo
 import time
 with st.spinner("Loading....."):
        time.sleep(5) 
-conn=pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.2")
+conn=pymongo.MongoClient("mongodb+srv://sk3538970_db_user:Yn1qjlwb3oJ4z6K1@pyversedb.bxahnpm.mongodb.net/?appName=PyverseDB")
 mydb=conn["ojt"]
 my=mydb["user_info"]
 st.markdown("""
