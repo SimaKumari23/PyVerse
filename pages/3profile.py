29207cfb80d58edc3b7b22831ac1332201877629
SimaKumari23 <sk3538970@gmail.com>
2026-08-03 00:53:50 +0530

    Update 3profile.py

--- pages/3profile.py
+++ pages/3profile.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_extras.stylable_container import stylable_container
 import pymongo
-conn = pymongo.MongoClient("mongodb+srv://sk3538970_db_user:Yn1qj1wb3oJ4z6K1@pyversedb.bxahnpm.mongodb.net/ojt?retryWrites=true&w=majority")
+conn = pymongo.MongoClient("mongodb+srv://sk3538970_db_user:Yn1qjlwb3oJ4z6K1@pyversedb.bxahnpm.mongodb.net/ojt?retryWrites=true&w=majority")
 mydb=conn["ojt"]
 my=mydb["user_info"]
 st.set_page_config(page_title="My Profile", page_icon="👤", layout="wide")   
