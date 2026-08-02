c1951596d5c9375f95c080bcd42eb83cebaabfb2
SimaKumari23 <sk3538970@gmail.com>
2026-08-03 00:53:04 +0530

    Update 2SignUp.py

--- pages/2SignUp.py
+++ pages/2SignUp.py
@@ -5,7 +5,7 @@ import smtplib
 server=smtplib.SMTP("smtp.gmail.com",587)
 server.starttls()
 server.login("sk3538970@gmail.com","ccaxauouqcrinfzo")
-conn = pymongo.MongoClient("mongodb+srv://sk3538970_db_user:Yn1qj1wb3oJ4z6K1@pyversedb.bxahnpm.mongodb.net/ojt?retryWrites=true&w=majority")
+conn = pymongo.MongoClient("mongodb+srv://sk3538970_db_user:Yn1qjlwb3oJ4z6K1@pyversedb.bxahnpm.mongodb.net/ojt?retryWrites=true&w=majority")
 mydb=conn["ojt"]#database of mongodb
 my=mydb["user_info"]#it is collection(table)
 st.markdown("""
